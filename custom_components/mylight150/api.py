d1947cb15ecfa64477e7c9541b16ab09e3a1f58d
Racailloux <racailloux@sheepy.fr>
2026-09-12 22:04:38 +0200

    add: 30 seconds margin to avoid token expiration during API calls

--- custom_components/mylight150/api.py
+++ custom_components/mylight150/api.py
@@ -71,7 +71,8 @@ class MyLight150ApiClient:
 
     async def async_get_token(self) -> str:
         """Return a valid access token, or None if failed. Refreshing or re-logging if needed."""
-        now = datetime.now(timezone.utc).timestamp()
+        # 30 seconds margin to avoid token expiration during API calls
+        now = datetime.now(timezone.utc).timestamp() - 30 
         _LOGGER.debug(
             f"Current tokens validities: (current time: {now}) "
             f"Access token: {self._token_expires_at} / "
