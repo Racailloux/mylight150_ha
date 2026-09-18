b18bccb792c7d3889e21486205d1c3615844ce7c
Racailloux <racailloux@sheepy.fr>
2026-09-18 20:59:28 +0200

    add: cleanup process at startup to remove orphan devices.

--- custom_components/mylight150/__init__.py
+++ custom_components/mylight150/__init__.py
@@ -24,9 +24,9 @@ _LOGGER = logging.getLogger(__name__)
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     # API instanciation
     api = MyLight150ApiClient(
+        hass=hass,
         username=entry.data[CONF_USERNAME],
         password=entry.data[CONF_PASSWORD],
-        hass=hass,
     )
 
     # Coordinator instantiation (pooling time comes from option in config entry)
@@ -41,6 +41,11 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     # Restore long term data & First refresh
     await coordinator.async_load_persistent_data()
     await coordinator.async_config_entry_first_refresh()
+
+    # Cleanup all historical devices (old identifier) associated with this config entry
+    await _async_cleanup_orphan_devices(
+        hass, entry, IDENTIFIER
+    )
 
     # Storing coordinator into hass.data to be acccessible from sensor.py
     hass.data.setdefault(DOMAIN, {})
@@ -70,3 +75,27 @@ async def _async_update_listener(hass: HomeAssistant, entry: ConfigEntry) -> Non
     # Called when any option changed (refresh delay time, etc.)
     _LOGGER.debug("Mofidied options, reloading integration.")
     await hass.config_entries.async_reload(entry.entry_id)
+
+
+async def _async_cleanup_orphan_devices(
+    hass: HomeAssistant, entry: ConfigEntry, valid_identifiers: str | None
+) -> None:
+    """Delete orphaned devices (old identifier) associated with this config entry."""
+    device_registry = dr.async_get(hass)
+    entity_registry = er.async_get(hass)
+    devices = dr.async_entries_for_config_entry(device_registry, entry.entry_id)
+
+    expected = (DOMAIN, valid_identifiers)
+                
+    for device in devices:
+        if device.identifiers == expected:
+            continue
+
+        # Never delete a device that still has active entities
+        if er.async_entries_for_device(entity_registry, device.id, include_disabled_entities=True):
+            _LOGGER.info(f"Device '{device.name}' does not correspond to any expected identifier but still has entities, not deleting.")
+            continue
+
+        _LOGGER.debug(f"Removing orphan device {device.id} (identifiers={device.identifiers})")
+        #device_registry.async_remove_device(device.id)
+
